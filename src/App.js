6608b389b20685bc204146a96c8a7440015b8268
vai na web <vainawebcpx@gmail.com>
2019-12-18 13:12:55 -0300

    Fiz o primeiro desafio do módulo 02 (Obs: eu ainda não o tinha feito), a pedido do prof Cícero. Falta: acertar a posição da imagem do background da pág. inicial. RJ, 18 dez 2019 - quarta-feira.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Frontpage from './placeofcomponent/Frontpage';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Frontpage/>
+      </div>
+    );
+  }
+}
+
+export default App;
